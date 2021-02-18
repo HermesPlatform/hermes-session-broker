5408ec77398d4b9ebc7801c1beb5be37c61b1d16
ynngdd <yaskata123@gmail.com>
2021-02-18 22:35:48 +0100

    feat: started implementing persistent models

--- hermes-core/app/models/User.scala
+++ hermes-core/app/models/User.scala
@@ -1,5 +1,17 @@
 package models
 
-class User {
+import org.hibernate.id.CompositeNestedGeneratedValueGenerator.GenerationPlan
 
+import java.util.Date
+import javax.persistence.{Entity, GeneratedValue, GenerationType, Id}
+
+@Entity(name = "User")
+class User(
+      var username: String,
+      var passwordHash: String,
+      var dateCreated: Date,
+      var currentIp: String){
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  var id: Long = _
 }
